909515426495d585e2ac2c00dea6fcdb68e3342e
Keisuke Inoue <kskinoue@gmail.com>
2024-06-17 19:57:24 +0900

    iRIC v4 (develop) Anaconda environment updated

--- dev_v4_src/packages/miniconda.main/data/miniconda_install.bat
+++ dev_v4_src/packages/miniconda.main/data/miniconda_install.bat
@@ -1,4 +1,4 @@
 @echo off
 mkdir %2
-"%1\Miniconda3-py38_4.9.2-Windows-x86_64.exe" /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%2
-%2\condabin\conda.bat create -p %2\envs\iric python=3.8 numpy numba scipy gdal h5py pandas matplotlib pillow requests beautifulsoup4 -y -q
+"%1\Miniconda3-py312_24.4.0-0-Windows-x86_64.exe" /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%2
+%2\condabin\conda.bat create -p %2\envs\iric python=3.12 numpy=1.26.4 numba=0.59.1 scipy=1.13.1 gdal=3.6.2 h5py=3.11.0 pandas=2.2.2 matplotlib=3.8.4 requests=2.32.2 beautifulsoup4=4.12.3 -y -q
--- dev_v4_src/packages/miniconda.main/meta/installscript.qs
+++ dev_v4_src/packages/miniconda.main/meta/installscript.qs
@@ -10,5 +10,5 @@ Component.prototype.createOperations = function()
 
 	component.addOperation("Execute", "@TargetDir@/miniconda_install.bat", "@TargetDir@", "@TargetDir@\\Miniconda3", "UNDOEXECUTE", "@TargetDir@/miniconda_uninstall.bat", "@TargetDir@\\Miniconda3");
 
-	component.addOperation("Delete", "@TargetDir@/Miniconda3-py38_4.9.2-Windows-x86_64.exe");
+	component.addOperation("Delete", "@TargetDir@/Miniconda3-py312_24.4.0-0-Windows-x86_64.exe");
 }
