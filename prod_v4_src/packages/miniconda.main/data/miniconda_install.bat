93f1b0e11749130db4cf15e52c828e9f50844ac7
Keisuke Inoue <keisuke.inoue@mizuho-ir.co.jp>
2025-10-09 15:43:35 +0900

    miniconda packages updated.
    conda update conda -y
    を追加

--- prod_v4_src/packages/miniconda.main/data/miniconda_install.bat
+++ prod_v4_src/packages/miniconda.main/data/miniconda_install.bat
@@ -1,4 +1,5 @@
 @echo off
 mkdir %2
 "%1\Miniconda3-py312_24.4.0-0-Windows-x86_64.exe" /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%2
+%2\condabin\conda.bat update conda -y
 %2\condabin\conda.bat update --all -y
