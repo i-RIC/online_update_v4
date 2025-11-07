129b11379f5b2f486228dc8ad929e7165dac523c
Keisuke Inoue <kskinoue@gmail.com>
2025-11-07 18:12:39 +0900

    Dev 版 miniconda.main パッケージを更新 conda-forge を使うようにした

--- dev_v4_src/packages/miniconda.main/data/miniconda_install.bat
+++ dev_v4_src/packages/miniconda.main/data/miniconda_install.bat
@@ -1,5 +1,8 @@
 @echo off
 mkdir %2
 "%1\Miniconda3-py312_24.4.0-0-Windows-x86_64.exe" /InstallationType=JustMe /RegisterPython=0 /AddToPath=0 /S /D=%2
-%2\condabin\conda.bat update conda -y
-%2\condabin\conda.bat update --all -y
+%2\condabin\conda.bat init
+%2\condabin\conda.bat condig --remove channels defaults
+%2\condabin\conda.bat condig --add channels conda-forge
+%2\condabin\conda.bat update -n base conda -y
+%2\condabin\conda.bat update -n base --all -y
