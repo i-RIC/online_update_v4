216983fb879071d4dcdd77f7d33e8f1e2194b7f9
Keisuke Inoue <kskinoue@gmail.com>
2026-06-15 13:37:25 +0900

    RRI_demAdjust2 registered.

---
+++ prod_v4_src/packages/gridcreator.RRI_demAdjust2/data/translation_ja_JP.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TS version="2.0" language="ja_JP">
+  <context>
+    <message>
+      <source>RRI DEM and River Grid Creator</source>
+      <translation>RRI DEM・河道格子生成</translation>
+    </message>
+    <message>
+      <source>Topographic Data</source>
+      <translation>地形データ</translation>
+    </message>
+    <message>
+      <source>Coordinate System</source>
+      <translation>座標系</translation>
+    </message>
+    <message>
+      <source>Longitude / Latitude</source>
+      <translation>経度・緯度</translation>
+    </message>
+    <message>
+      <source>Projected Coordinates (meter)</source>
+      <translation>投影座標（メートル）</translation>
+    </message>
+    <message>
+      <source>DEM File</source>
+      <translation>DEMファイル</translation>
+    </message>
+    <message>
+      <source>DIR File</source>
+      <translation>DIRファイル</translation>
+    </message>
+    <message>
+      <source>ACC File</source>
+      <translation>ACCファイル</translation>
+    </message>
+    <message>
+      <source>River Geometry</source>
+      <translation>河道形状</translation>
+    </message>
+    <message>
+      <source>River Cell ACC Threshold</source>
+      <translation>河道セル判定ACC</translation>
+    </message>
+    <message>
+      <source>River Width</source>
+      <translation>川幅</translation>
+    </message>
+    <message>
+      <source>River Depth</source>
+      <translation>河道深</translation>
+    </message>
+    <message>
+      <source>Levee Height</source>
+      <translation>堤防高</translation>
+    </message>
+    <message>
+      <source>Height [m]</source>
+      <translation>高さ [m]</translation>
+    </message>
+    <message>
+      <source>Minimum ACC for Height</source>
+      <translation>高さを設定する最小ACC</translation>
+    </message>
+    <message>
+      <source>Grid generation failed. Check the input raster files and parameters.</source>
+      <translation>格子生成に失敗しました。入力ラスターファイルとパラメータを確認してください。</translation>
+    </message>
+  </context>
+</TS>
